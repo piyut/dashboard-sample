13fdc562c69b7bde4c2de3ad06bd1258ec212742
mahroni <mahroni87@gmail.com>
2017-11-15 10:12:58 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,120 @@
+var page = 0;
+    itemsPerPage = 10;
+    avatar = $('#avatar');
+
+getDataFromApi(page);
+
+function getDataFromApi(page) {
+    var url = 'https://wory.qiscus.com/api/v2.1/rest/get_user_list';
+    $("<div class='box-loading'><span class='icon-loading'></span> LOADING</div>").appendTo( ('.box-table') );
+
+    $.ajax({
+        url: url,
+        type: 'get',
+        data: {
+            limit: itemsPerPage,
+            page: page
+        },
+        headers: {
+            QISCUS_SDK_SECRET: 'bc01ec2b15588369317421bfb25a44bf',
+            'Content-Type':'application/x-www-form-urlencoded'
+        },
+        dataType: 'json',
+        success: function (data) {
+            $('table > tbody').empty();
+            $('.box-loading').remove();
+            listingData(data, page);
+        }
+    });
+}
+
+function listingData(data, page) {
+    if ($("table > tbody").children().length > 0) {
+        $("table > tbody").empty();
+    }
+    if (data.results.users.length > 0) {
+        $.each(data.results.users, function (index, val) {
+            var key = ((page - 1) * itemsPerPage) + (index + 1);
+            var username = val.username ? val.username : '-';
+            var createDate = DateFormat.format.date(val.created_at, 'dd/MM/yyyy HH:mm:ss')
+            var updateDate = DateFormat.format.date(val.updated_at, 'dd/MM/yyyy HH:mm:ss')
+            $("<tr><th scope='row'>" + key + "</th><td class='text-capitalize'><img style='margin-right: 10px;' class='img-circle' width='30' height='30' src=" + val.avatar_url + ">" + username + "</td><td>" + val.id + "</td><td>" + val.email + "</td><td>" + createDate + "</td><td>" + updateDate + "</td></tr>").appendTo( ('tbody') );
+        });
+        $('#pagination').twbsPagination({
+            totalPages: Math.ceil(data.results.meta.total_data / itemsPerPage),
+            onPageClick: function (evt, page) {
+                page = page;
+                getDataFromApi(page);
+            }
+        });
+    } else {
+        $("<tr></tr><tr><td colspan='6' class='text-center'><div class='icon-empty-user'></div><div class='info-empty'>User Data Not Found</div><div class='instruction-empty'>You can add user to use it on your app that using Qiscus SDK</div><div><button type='button' class='btn btn-default' data-toggle='modal' data-target='#createUserModal'><span class='icon-user'></span> Add User </button></div></td></tr>").appendTo( ('tbody') );
+    }
+}
+
+/**
+ * create new user
+ */
+window.URL = window.URL || window.webkitURL;
+function handleFiles(files) {
+    for (var i = 0; i < files.length; i++) {
+        avatar.attr('src', window.URL.createObjectURL(files[i]));
+        var info = document.createElement("div");
+        info.innerHTML = "Size: " + files[i].size + " bytes";
+        avatar.parent().append(info);
+    }
+    avatar = files[0];
+}
+
+$('input,textarea').on('keyup change keypress', function () {
+    var send        = $('#buttonCreateUser')
+    if ($('input#email').val() != '' && $('input#username').val() != '' && $('input#password').val() != '') {
+        send.removeClass('disable')
+    } else {
+        send.addClass('disable')
+    }
+})
+
+$('#buttonCreateUser').on("click", function () {
+    /*
+        ** this url is your app code get from https://www.qiscus.com/dashboard
+        ** more info about create user https://www.qiscus.com/documentation/rest/list-api
+    */
+    var self = $('#buttonCreateUser')
+    var email = $('#email').val();
+    var password = $('#password').val();
+    var username = $('#username').val();
+    var avatar_url = $('#avatar_url').val();
+    self.empty();
+    self.css('background', '#F2994A');
+    self.append("<span class='icon-loading icon-loading-white'></span> Creating User");
+    self.addClass('disabled');
+    var url = 'https://wory.qiscus.com/api/v2/rest/login_or_register'
+    $.ajax({
+        url: url,
+        method: 'POST',
+        type: 'POST',
+        data: {
+            email: email,
+            password: password,
+            username: username,
+            avatar_url: avatar_url
+        },
+        headers: {
+            QISCUS_SDK_SECRET: 'bc01ec2b15588369317421bfb25a44bf',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        dataType: 'json',
+        success: function (data) {
+            $('#createUserModal').modal('hide')
+            setTimeout(function () {
+                location.reload()
+            }, 1000);
+        },
+        error: function (error) {
+            self.empty();
+            self.append('Add User')
+            self.css('background', '#2ACB6E');
+        }
+    });
+});
