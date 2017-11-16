2d337a0785e0d54513293be382e15a2648c2d5b2
octopus <ahyana1995@gmail.com>
2017-11-16 14:28:20 +0700

    add sample json response

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-<?php include_once("home.html"); ?>
+<?php
+
+include __DIR__ . '/vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Phroute\Phroute\RouteCollector;
+use Phroute\Phroute\Dispatcher;
+
+$app_id = 'check';
+$secret_key = 'rahasiabanget';
+$base_url = 'https://' . $app_id . '.qiscus.com/';
+
+$client = new Client(['base_uri' => $base_url]);
+$router = new RouteCollector();
+$router->get('/', function (){
+    require_once "home.html";
+});
+$router->get('/rest/contact_list', function () use($client, $app_id, $secret_key){
+    $httpResp = $client->request('GET', '/api/v2.1/rest/get_user_list', [
+        'multipart' => [],
+        'headers' => [
+            'Accept' => 'application/json',
+            'QISCUS_SDK_APP_ID' => $app_id,
+            'QISCUS_SDK_SECRET' => $secret_key
+        ]
+    ]);
+    header('Content-Type: application/json');
+    return $httpResp->getBody();
+});
+
+
+$dispatcher =  new Dispatcher($router->getData());
+$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+// Print out the value returned from the dispatched function
+echo $response;
