e156bf20d3ea4f31bbcd93ea9c803a5b39d5540f
octopus <ahyana1995@gmail.com>
2017-11-17 17:05:26 +0700

    using klein as router

--- index.php
+++ index.php
@@ -5,44 +5,16 @@ use GuzzleHttp\Client;;
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
+$route = new \Klein\Klein();
 
-$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
-    $r->addRoute('GET', '/', 'homepage');
-    $r->addRoute('GET', '/contacts', 'contacts');
+$route->respond('GET', '/', function(){
+   return require_once 'home.html';
 });
-
-// Fetch method and URI from somewhere
-$httpMethod = $_SERVER['REQUEST_METHOD'];
-$uri = $_SERVER['REQUEST_URI'];
-
-// Strip query string (?foo=bar) and decode URI
-if (false !== $pos = strpos($uri, '?')) {
-    $uri = substr($uri, 0, $pos);
-}
-$uri = rawurldecode($uri);
-
-$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
-switch ($routeInfo[0]) {
-    case FastRoute\Dispatcher::NOT_FOUND:
-        // ... 404 Not Found
-        break;
-    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-        $allowedMethods = $routeInfo[1];
-        // ... 405 Method Not Allowed
-        break;
-    case FastRoute\Dispatcher::FOUND:
-        $handler = $routeInfo[1];
-        $vars = $routeInfo[2];
-        switch ($handler) {
-            case 'homepage':
-                require_once 'home.html';
-                break;
-            case 'contacts':
-                getContactList();
-                break;
-        }
-        break;
-}
+$route->respond('GET', '/rest/contacts', function(){
+    $contacts = getContactList();
+    return $contacts;
+});
+$route->dispatch();
 
 function getContactList() {
     $call = callHttp("/api/v2.1/rest/get_user_list");
