9b0375c44723b998e61952ca49ba8ee99f4fb7b2
octopus <ahyana1995@gmail.com>
2017-11-17 16:22:34 +0700

    using fast php router

--- index.php
+++ index.php
@@ -1,39 +1,66 @@
 <?php
+require 'vendor/autoload.php';
+use GuzzleHttp\Client;;
 
-require('vendor/autoload.php');
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
 
-use GuzzleHttp\Client;
-use Phroute\Phroute\RouteCollector;
-use Phroute\Phroute\Dispatcher;
 
-$app_id = 'sampleapp-65ghcsaysse';
-$secret_key = 'dc0c7e608d9a23c3c8012c6c8572e788';
-$base_url = 'https://' . $app_id . '.qiscus.com/';
-
-$client = new Client(['base_uri' => $base_url]);
-$router = new RouteCollector();
-$router->get('/', function (){
-    require_once "home.html";
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', 'homepage');
+    $r->addRoute('GET', '/contacts', 'contacts');
 });
-$router->get('/rest/contact_list', function () use($client, $app_id, $secret_key){
-    $httpResp = $client->request('GET', '/api/v2.1/rest/get_user_list', [
-        'multipart' => [],
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        switch ($handler) {
+            case 'homepage':
+                require_once 'home.html';
+                break;
+            case 'contacts':
+                getContactList();
+                break;
+        }
+        break;
+}
+
+function getContactList() {
+    $call = callHttp("/api/v2.1/rest/get_user_list");
+    header('Content-Type: application/json');
+    echo $call;
+}
+
+function callHttp($url, $params = []){
+    $base_url = 'https://' . getenv('APP_ID') . '.qiscus.com/';
+    $client = new Client(['base_uri' => $base_url]);
+    $httpResp = $client->request('GET', $url, [
+        'multipart' => $params,
         'headers' => [
             'Accept' => 'application/json',
-            'QISCUS_SDK_APP_ID' => $app_id,
-            'QISCUS_SDK_SECRET' => $secret_key
+            'QISCUS_SDK_APP_ID' => getenv('APP_ID'),
+            'QISCUS_SDK_SECRET' => getenv('SECRET_KEY')
         ]
     ]);
-    header('Content-Type: application/json');
+
     return $httpResp->getBody();
-});
-
-$router->get('/test', function(){
-    return 'this is test';
-});
-
-$dispatcher =  new Dispatcher($router->getData());
-$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-
-// Print out the value returned from the dispatched function
-echo $response;
+}
