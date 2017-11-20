68f4c1ca6e3fc63a1ce9912824c5d49d4f1d7e43
octopus <ahyana1995@gmail.com>
2017-11-20 16:04:50 +0700

    add pagination for contacts

--- index.php
+++ index.php
@@ -10,25 +10,48 @@ $route = new \Klein\Klein();
 $route->respond('GET', '/', function(){
    return require_once 'home.html';
 });
-$route->respond('GET', '/rest/contacts', function(){
+
+$route->respond('GET', '/rest/contacts', function($request){
+    if(empty($request->page) || $request->page == 0 ) {
+        $request->page = 1;
+    }
+    if(empty($request->limit) || $request->limit == 0) {
+        $request->limit = 20;
+    }
     $contacts = getContactList();
     return $contacts;
 });
-$route->respond('POST', '/rest/upload', function(){
-    $file = uploadPhoto();
+$route->respond('POST', '/rest/upload', function($request){
+    if(empty($_FILES['file']['tmp_name'])){
+        header('Content-Type: application/json', true, 400);
+        return json_encode(['file'=>'required']);
+    }
+    $file = uploadPhoto($_FILES['file']['tmp_name']);
     return $file;
 });
 $route->dispatch();
 
-function getContactList() {
-    $call = callHttp("/api/v2.1/rest/get_user_list");
+function getContactList($page, $limit) {
+    $call = callHttp("/api/v2.1/rest/get_user_list", [
+        [
+            'name'=> 'page',
+            'contents' => $limit,
+        ], [
+            'name' => 'limit',
+            'limit' => $limit,
+        ]
+    ]);
     header('Content-Type: application/json');
     echo $call;
 }
 
-function uploadPhoto() {
-    $photo = callHttp("/api/v2/sdk/upload");
-    header('Content-Type: application/json');
+function uploadPhoto($file) {
+    $photo = callHttp("/api/v2/sdk/upload",[
+        [
+            'name' => 'file',
+            'contents' => $file,
+        ]
+    ]);
     echo $photo;
 }
 
